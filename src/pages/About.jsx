abcce7dc04a74f65014040c0a21803b81ad78cf2
Peggy Condon <430056+peggyc3@users.noreply.github.com>
2022-05-18 16:03:02 -0400

    added nav, footer, pages

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+function About() {
+  return (
+    <>
+      <p className="mb-4 px-6 text-1xl font-light">
+        This is a React app with Tailwind CSS to search GitHub profiles and see
+        profile details. The project uses Daisy UI components.
+      </p>
+    </>
+  );
+}
+
+export default About;
