689080616595dcf4d4c7ac8ef60c7da31463ff56
Peggy Condon <430056+peggyc3@users.noreply.github.com>
2022-05-19 08:22:56 -0400

    added Git api credentials

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,8 @@ function About() {
     <>
       <p className="mb-4 px-6 text-1xl font-light">
         This is a React app with Tailwind CSS to search GitHub profiles and see
-        profile details. The project uses Daisy UI components.
+        profile details. The project also uses Daisy UI components. Thanks to
+        Brad Traversy for all of his excellent tutorials!
       </p>
     </>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Home() {
   return (
     <div>
-      <h1 className="text-6xl">Welcome</h1>
+      <h1 className="text-6xl flex-auto text-center">Welcome</h1>
     </div>
   );
 }
